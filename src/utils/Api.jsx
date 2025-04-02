1ee62dd4af12be453ae25564f41f3e4088fec39b
kishan-71 <kishanm.pgvcl@gmail.com>
2025-04-02 10:17:29 +0530

    update uith DataForm

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost/medicalbill/api/", // Replace with your API endpoint
+    baseURL: "http://localhost/medical-bill/api/", // Replace with your API endpoint
     headers: {
       "Content-Type": "application/json",
     },
